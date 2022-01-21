00538834dfe26392c46f17edd01bc5b91e555861
Williams Andrade <wilandradeb@gmail.com>
2022-01-20 21:23:43 -0300

    ExerciciosUdemy

---
+++ udemy/src/curso/PrimeiroExemplo.java
@@ -0,0 +1,17 @@
+package curso;
+
+public class PrimeiroExemplo {
+
+	/* Exibe mensagem Olá Mundo! */
+	public void exibeMensagem() {
+		System.out.println("Ola Mundo!");
+
+	}
+
+	public static void main(String[] args) {
+
+		PrimeiroExemplo pe = new PrimeiroExemplo();
+		pe.exibeMensagem();
+	}
+
+}
